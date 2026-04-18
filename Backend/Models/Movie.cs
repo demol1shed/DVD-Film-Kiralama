c02adde74ad735f26b2c14b5099f8eef42ce6218
Arda <cosardemirarda@gmail.com>
2026-04-19 02:57:02 +0300

    database olusturuldu

---
+++ Backend/Models/Movie.cs
@@ -0,0 +1,30 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace Backend.Models
+{
+    public class Movie
+    {
+        [Key]
+        public uint Id{get; set;}
+        public required string MovieName{get; set;} // required = null olamaz
+        public DateTime ReleaseDate{get; set;}
+        public required string Genre{get; set;} // required = null olamaz
+        public uint TotalBorrowCount{get; set;}
+        public DateTime TimeNotBorrowed{get; set;}
+
+        public double Popularity
+        {
+            get
+            {
+                var timeInShelf = (DateTime.Now - TimeNotBorrowed).TotalDays;
+                double logVal = Math.Log(timeInShelf + 1);
+
+                // log degeri 0 dan buyukse odunc alim orani / log, degilse 0.
+                return logVal > 0 ? TotalBorrowCount / logVal : 0;
+            }
+        }
+    }
+}
---
+++ Backend/Program.cs
@@ -0,0 +1,21 @@
+﻿using Backend.Context;
+using Backend.Models;
+
+/*using (var context = new DvdContext())
+{
+    var movieCount = context.Movies.Count();
+    Console.WriteLine(movieCount);
+
+    if(movieCount == 0)
+    {
+        var testMovie = new Movie{
+            MovieName = "Inception",
+            Genre = "Sci-Fi",
+            ReleaseDate = DateTime.Now,
+            TimeNotBorrowed = DateTime.Now
+        };
+        context.Movies.Add(testMovie);
+        context.SaveChanges();
+        Console.WriteLine("test filmi eklendi");
+    }
+}*/
