0c3d7f8f6460ec829a576bc32b2ecc2df9673949
Arda <cosardemirarda@gmail.com>
2026-04-19 20:44:36 +0300

    backend frontend baglantisi kuruldu, json deserialization yapiliyor

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.Json;
 using SharedLib;
 
 class Program
@@ -13,7 +14,15 @@ class Program
         Console.WriteLine("Json talebi geldi: " + jsonData);
 
         // TODO: json'u deserialize et, bilgileri hashle, db'den kontrol et.
+        SignInRequest? signInInfo = JsonSerializer.Deserialize<SignInRequest>(jsonData);
 
-        return "backend'e veri girisi basarili";
+        if(signInInfo == null)
+        {
+            return "null data geldi";
+        }
+        else
+        {
+            return $"gelen isim {signInInfo.Username}";
+        }
     }
 }
